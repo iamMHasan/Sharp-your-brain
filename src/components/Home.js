cb064cf1a491a719148fbd45f2b5876c6000036b
iamMHasan <abcimhasanbro@gmail.com>
2022-10-10 21:18:33 +0600

    nav added

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { HeaderBanner } from './HeaderBanner';
+
+const Home = () => {
+    return (
+        <div>
+            <HeaderBanner></HeaderBanner>
+            <h1>this is home</h1>
+        </div>
+    );
+};
+
+export default Home;
