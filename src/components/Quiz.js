b9a79bc55bfd2fbc69bdc4aeee26c744950c49d1
iamMHasan <abcimhasanbro@gmail.com>
2022-10-11 09:21:38 +0600

    quiz logic added

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,14 +1,31 @@
 import React from 'react';
+import { toast } from 'react-toastify';
 
-const Quiz = ({question}) => {
-    const {question: ques} = question
+const Quiz = ({ question }) => {
+    const { question: ques } = question
     console.log(question);
+    const handleQuiz = correctAns => {
+        console.log(correctAns);
+        if (correctAns === question.correctAnswer) {
+           toast.success('correct answer')
+           alert('correct answer')
+        } else{
+            alert('wrong answer')
+        }
+
+
+    }
     return (
-        <div className='m-2'>
+        <div className='m-2 p-3 border-neutral-500 border border-2 rounded'>
             <p className='text-xl text-center'>{ques}</p>
             {
-                question.options.map(quiz => (
-                    <button className='bg-slate-200 m-3'>
+                question.options.map((quiz, index) => (
+
+                    <button
+                        key={index}
+                        // onClick={handleQuiz}
+                        onClick={() => { handleQuiz(quiz) }}
+                        className='bg-slate-200 hover:bg-slate-300 m-3 p-3 rounded text-bold'>
                         {quiz}
                     </button>
                 ))
--- src/components/Quizplay.js
+++ src/components/Quizplay.js
@@ -7,14 +7,13 @@ const Quizplay = () => {
     const questions = useLoaderData().data.questions
     const {name} = quiz
 
-    // console.log(questions);
-    // console.log(quiz);
+    // console.log(questions)
     return (
         <div className='text-center w-9/12 mx-auto'>
            <h2 className='text-2xl text-center font-bold'>start the quiz of {name}</h2>
            <div className="grid grid-cols-2 gap-4 bg-slate-300 ">
                 {
-                    questions.map(question => <Quiz question={question}></Quiz>)
+                    questions.map(question => <Quiz key={question.id} question={question}></Quiz>)
                 }
            </div>
         </div>
